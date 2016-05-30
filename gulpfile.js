f137464906b343f25ffe0aaa71957ed13d66e93a
“David <david@camposgoticos.com>
2016-05-30 22:55:47 +0200

    ajuste gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ var less = require('gulp-less');
 var path = require('path');
  
 gulp.task('less', function () {
-  return gulp.src('./less/**/*.less')
+  return gulp.src('./src/css/*.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-    .pipe(gulp.dest('./public/css'));
+    .pipe(gulp.dest('./dest/css'));
 });
