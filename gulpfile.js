32e8ec61608491bf9e20f8c9341831362fd04096
David Paramio <david@camposgoticos.com>
2016-06-01 10:26:59 +0200

    gulpfile updated(tarea default error de sintaxis)

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var less = require('gulp-less');
 var path = require('path');
- 
+var gulp = require('gulp');  
+var browserSync = require('browser-sync');  
+
 gulp.task('less', function () {
   return gulp.src('./src/css/*.less')
     .pipe(less({
@@ -9,16 +11,18 @@ gulp.task('less', function () {
     .pipe(gulp.dest('./dest/css'));
 });
 
-var gulp = require('gulp');  
-var browserSync = require('browser-sync');  
+
 gulp.task('watch', function () {  
   gulp.watch('src/css/*.less', ['css', browserSync.reload]);
   gulp.watch('src/js/*.js', ['js', browserSync.reload]);
   gulp.watch('src/*.html', ['html', browserSync.reload]);
 });
+
 gulp.task('browser-sync', function() {  
   browserSync({
     proxy: "http://localhost:2368"
-  });
 });
+
+gulp
+gulp.task('default' , ['less','watch','browser-sync']);
 
