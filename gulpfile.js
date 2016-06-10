db04755b7795cb0112b1ed36c5cbcaad8780fb62
David Paramio <david@camposgoticos.com>
2016-06-10 12:56:40 +0200

    imagenes añadidas

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('copy-img', function(){
   return gulp.src('src/img/*')
     .pipe(gulp.dest('./dest/img/'));
 });
-gulp.task('copy-js', function(){
+gulp.task('js', function(){
   return gulp.src('src/js/*')
     .pipe(gulp.dest('./dest/js/'));
 });
@@ -42,6 +42,6 @@ gulp.task('html', function() {
     .pipe(gulp.dest('dest/'))
 });
 
-gulp.task('default' , ['watch','less', 'copy-js', 'copy-img','html','browser-sync']);
+gulp.task('default' , ['watch','less', 'js', 'copy-img','html','browser-sync']);
 
 
